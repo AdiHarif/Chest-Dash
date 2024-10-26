6b935022ae6af31d961d8b636396eab3688020b4
Adi Harif <adi.harif@campus.technion.ac.il>
2024-10-26 12:28:21 +0300

    Aligns ui buttons to the box center

--- src/ui.rs
+++ src/ui.rs
@@ -15,10 +15,11 @@ pub fn initialize_ui() {
 
 pub fn show_start_button() -> bool {
     let dimensions = root_ui().calc_size("Start");
+    let tile_size = get_tile_size();
     return root_ui().button(
         Vec2 {
-            x: (screen_width() - dimensions.x) / 2.0,
-            y: (screen_height() - dimensions.y) / 2.0,
+            x: (GRID_COLS_COUNT as f32 * tile_size - dimensions.x) / 2.0,
+            y: (GRID_ROWS_COUNT as f32 * tile_size - dimensions.y) / 2.0,
         },
         "Start",
     );
@@ -26,9 +27,12 @@ pub fn show_start_button() -> bool {
 
 pub fn show_game_over_button(label: &String) -> bool {
     let label_dimensions = root_ui().calc_size(&label);
+    let tile_size = get_tile_size();
+    let box_width = GRID_COLS_COUNT as f32 * tile_size;
+    let box_height = GRID_ROWS_COUNT as f32 * tile_size;
     let label_position = Vec2 {
-        x: (screen_width() - label_dimensions.x) / 2.0,
-        y: (screen_height() / 2.0) - label_dimensions.y,
+        x: (box_width - label_dimensions.x) / 2.0,
+        y: (box_height / 2.0) - label_dimensions.y,
     };
     draw_rectangle(
         label_position.x,
@@ -41,8 +45,8 @@ pub fn show_game_over_button(label: &String) -> bool {
     let button_label = "Click here to restart";
     let button_dimensions = root_ui().calc_size(&button_label);
     let button_position = Vec2 {
-        x: (screen_width() - button_dimensions.x) / 2.0,
-        y: (screen_height() / 2.0),
+        x: (box_width - button_dimensions.x) / 2.0,
+        y: (box_height / 2.0),
     };
 
     root_ui().button(button_position, button_label)
